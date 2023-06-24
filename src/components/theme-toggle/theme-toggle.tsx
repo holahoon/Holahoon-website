5acc2ea3ece93eac91928c3a7a5905a6d172e5fa
DK <hola.hoon@gmail.com>
2023-06-24 23:33:51 +0900

    refactor: mode toggle

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -9,23 +8,13 @@ import { Icons } from "@/components/icons"
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme()
 
-  const [isMounted, setIsMounted] = useState<boolean>(false)
-
-  useEffect(() => setIsMounted(true), [])
-
-  if (!isMounted) return null
-
   return (
     <Button
       className="w-8 p-0"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "light" ? (
-        <Icons.sun className="absolute h-6 w-6 scale-100 transition-all dark:rotate-45 dark:scale-0" />
-      ) : null}
-      {theme === "dark" ? (
-        <Icons.moon className="absolute h-6 w-6 rotate-45 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      ) : null}
+      <Icons.sun className="absolute h-6 w-6 scale-100 transition-all dark:rotate-45 dark:scale-0" />
+      <Icons.moon className="absolute h-6 w-6 rotate-45 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Theme toggle</span>
     </Button>
   )
