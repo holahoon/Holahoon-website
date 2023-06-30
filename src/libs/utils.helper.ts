fbe3f83788f81c7647d4d99c9955291439a5eeb9
DK <hola.hoon@gmail.com>
2023-06-30 13:01:58 +0900

    feat: format date testing

--- src/libs/utils.helper.ts
+++ src/libs/utils.helper.ts
@@ -6,12 +6,14 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs))
 
 /** Formats dates */
-export const formatDate = (date: string, dist = 1): string => {
+// TODO: Need hours and minutes to be worked on
+export const formatDate = (date: string, dist = 3): string => {
+  const DATE_FORMAT = "LLL do yyy"
   const now = Date.now()
   const d = new Date(date)
 
   const timeDiff = (now - d.getTime()) / 1000
   const days = 60 * 60 * 24 * dist
   if (timeDiff < days) return formatDistanceToNow(d, { addSuffix: true })
-  return format(d, "LLL do yyy")
+  return format(d, DATE_FORMAT)
 }
---
+++ src/libs/utils/__test__/utils.helpers.test.ts
@@ -0,0 +1,26 @@
+import { formatDate } from "@/libs/utils.helper"
+
+describe("Helper function formatDate()", () => {
+  beforeAll(() => {
+    jest.useFakeTimers().setSystemTime(new Date("2023-03-24"))
+  })
+  afterAll(() => {
+    jest.runOnlyPendingTimers()
+    jest.useRealTimers()
+  })
+
+  it("should return Feb 1st 2023", () => {
+    const date = "2023-02-01"
+    expect(formatDate(date)).toBe("Feb 1st 2023")
+  })
+
+  it("should return Mar 24th 1991", () => {
+    const date = "03-24-1991"
+    expect(formatDate(date)).toBe("Mar 24th 1991")
+  })
+
+  it("should return 2 days ago", () => {
+    const date = "03-24-2023"
+    expect(formatDate(date)).toBe("about 9 hours ago")
+  })
+})
