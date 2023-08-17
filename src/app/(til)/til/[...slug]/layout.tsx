4b4ad4a836a7f52b44a3e15f17f3e45b717e810c
DK <hola.hoon@gmail.com>
2023-08-18 08:37:16 +0900

    feat: generate metadata tag

--- src/app/(til)/til/[...slug]/layout.tsx
+++ src/app/(til)/til/[...slug]/layout.tsx
@@ -1,12 +1,28 @@
-import { type ReactNode } from "react"
+import type { Metadata } from "next"
+import type { ReactNode } from "react"
+
+import { getTilFromParams } from "@/libs/til"
 
 interface TilSlugLayoutProps {
   children: ReactNode
+  params: { slug: string[] }
 }
 
-// TODO: needs dynamic generateMeta function
+export const generateMetadata = async (
+  props: TilSlugLayoutProps
+): Promise<Metadata> => {
+  const { params } = props
+  const til = await getTilFromParams(params)
+  if (!til) return {}
 
-export default function TilSlugLayout(props: TilSlugLayoutProps) {
+  return {
+    title: til.title,
+    description: til.description,
+    openGraph: {}, // TODO
+  }
+}
+
+export default async function TilSlugLayout(props: TilSlugLayoutProps) {
   const { children } = props
 
   return <article className="mb-2 mt-12">{children}</article>
