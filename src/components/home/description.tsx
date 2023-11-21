a771b25a191c3ffae94d83c9ba69589ad2ae55a7
DK <hola.hoon@gmail.com>
2023-11-21 09:42:44 -0500

    update main page

--- src/components/home/description.tsx
+++ src/components/home/description.tsx
@@ -5,7 +5,7 @@ export default function Description() {
     <div className="mt-6 border-l-2 pl-4 text-sm text-primary/80 md:mt-auto md:text-base">
       <p className="font-semibold">I&apos;m currently</p>
       <p>open for a new opportunity</p>
-      <p>residing in Seoul, South Korea</p>
+      <p>residing in Buford, GA, USA</p>
 
       <Social className="mt-2" />
     </div>
